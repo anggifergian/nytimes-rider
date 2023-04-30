58e8b1dcc53681d171c3188e4aa2deb9a23148ee
anggifergian <anggyfergian56@gmail.com>
2023-04-30 09:33:28 +0700

    Add models - NewsResponse, News, Media, MediaMetaData

---
+++ NewsReaderApp/Models/Media.swift
@@ -0,0 +1,27 @@
+//
+//  Media.swift
+//  NewsReaderApp
+//
+//  Created by Anggi Fergian on 30/04/23.
+//
+
+import Foundation
+
+struct Media: Decodable {
+    let type: String
+    let caption: String
+    let metaData: [MediaMetaData]
+    
+    enum CodingKeys: String, CodingKey {
+        case type
+        case caption
+        case metadata = "media-metadata"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        type = try container.decodeIfPresent(String.self, forKey: .type) ?? ""
+        caption = try container.decodeIfPresent(String.self, forKey: .caption) ?? ""
+        metaData = try container.decodeIfPresent([MediaMetaData].self, forKey: .metadata) ?? []
+    }
+}
